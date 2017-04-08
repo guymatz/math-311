cd0c4139e429f37dcda81f6ab6c4b8430cd3e442
Guy Matz <gmatz@matz.org>
2017-04-08 10:48:57 -0400

    latex-ified output

--- z.py
+++ z.py
@@ -4,6 +4,11 @@ import sys
 
 z = int(sys.argv[1])
 
+print("""
+\\begin{equation*}
+\\begin{array}{c|c|c}
+\\text{element} & \\text{powers} & \\text{order}
+""")
 for e in range(1,z):
     powers = []
     for n in range(0,z):
@@ -11,4 +16,11 @@ for e in range(1,z):
         element = str(p % z)
         if element not in powers:
             powers.append(element)
-    print("%i | {%s} | %i" % (e, ", ".join(powers), len(powers)))
+    print("\\\\")
+    print("\\hline")
+    print("%i & \{%s\} & %i" % (e, ", ".join(powers), len(powers)))
+    print
+print("""
+\end{array}
+\end{equation*}
+""")
